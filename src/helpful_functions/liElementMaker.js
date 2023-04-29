05c23f67715ed85db657fc06fb8eade74c813b4d
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-04-29 11:31:07 -0400

    Refactor Function To Include Href Links

--- src/helpful_functions/liElementMaker.js
+++ src/helpful_functions/liElementMaker.js
@@ -1,9 +1,10 @@
-export default function liElement(text, className, linkName) {
+export default function liElement(text, className, linkName, functionCall) {
   const liElement = document.createElement('li');
   liElement.setAttribute('class', `${className}-1`);
 
   const linkElement = document.createElement('a');
   linkElement.setAttribute('class', `${linkName}`);
+  linkElement.setAttribute('href', `${functionCall}`);
   linkElement.textContent = `${text}`;
 
   liElement.appendChild(linkElement);
